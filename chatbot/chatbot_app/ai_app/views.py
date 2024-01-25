4cff1f40c0df69d7acefccb2b6ecd2990f155d67
jroberto <jroberto@UBVPNN2815.core.unionbank.com>
2024-01-25 19:48:33 +0800

    added a new model in views.py

--- chatbot/chatbot_app/ai_app/views.py
+++ chatbot/chatbot_app/ai_app/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 import json
-import openai
+# import openai
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from chatbot_app.config import OPENAI_KEY, DB_NAME, USER, PASSWORD, HOST, PORT
+from chatbot_app.config import OPENAI_KEY, DB_NAME, USER, PASSWORD, HOST, PORT, MODEL_NAME
 import os
 import psycopg2
+import constants
 
-openai.api_key = OPENAI_KEY
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain_openai import ChatOpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+
+os.environ["OPENAI_API_KEY"] = constants.OPENAI_API_KEY
+
+# openai.api_key = OPENAI_KEY
 
 
 
@@ -69,17 +79,38 @@ def extract_data_from_database(connection, table_names):
    
     data_dict = {}
     for name in table_names:
-        cursor = connection.cursor()
-        query = f'Select * from {name} limit 3;'
-        cursor.execute(query)
-        data = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        result_as_dict = [dict(zip(column_names, map(str, row))) for row in data]
-        data_dict[name] = result_as_dict 
-        cursor.close()
+        if name == 'sharepoint_data':
+            cursor = connection.cursor()
+            query = f'Select * from {name} limit 20;'
+            cursor.execute(query)
+            data = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
+            result_as_dict = [dict(zip(column_names, map(str, row))) for row in data]
+            data_dict[name] = result_as_dict 
+            cursor.close()
 
     return str(data_dict)
 
+
+embeddings = OpenAIEmbeddings()
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+documents = os.path.join(base_dir, 'ai_app', 'documents')
+print(documents)
+
+
+loader = DirectoryLoader("documents", glob="**/*.txt", recursive = True)
+index = VectorstoreIndexCreator(vectorstore_cls=Chroma, 
+                                    embedding=embeddings, 
+                                    text_splitter=CharacterTextSplitter(separator = "\n",chunk_size=400, chunk_overlap=50),
+                                    vectorstore_kwargs={ "persist_directory": '/documents'}
+                                    ).from_loaders([loader])
+
+def third_model(user_message):
+    
+    response =  index.query(user_message, llm=ChatOpenAI())
+
+    return response
 
 
 def ai_response(request):
@@ -92,11 +123,12 @@ def ai_response(request):
 
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     kb_folder = os.path.join(base_dir, 'ai_app', 'kb')
-    file_path = os.path.join(kb_folder, 'step.txt')
+    # file_path = os.path.join(kb_folder, 'steps.txt')
 
-    with open(file_path, 'r', encoding='utf-8') as file:
-        steps = file.read()
+    # with open(file_path, 'r', encoding='utf-8') as file:
+    #     steps = file.read()
 
+    
 
     try:
         data = json.loads(request.body)
@@ -115,23 +147,28 @@ def ai_response(request):
     # Add the user message to the conversation history
     conversation_history.append({"role": "user", "content": user_message})
     
-    response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    # {"role": "system", "content": steps},
-                    {"role": "system", "content": tables_data},
-                    *conversation_history
-                ]
-            )
-    print(response['usage'])
+    # response = openai.ChatCompletion.create(
+    #             model=MODEL_NAME,
+    #             messages=[
+    #                 {"role": "system", "content": "You are a helpful assistant."},
+    #                 {"role": "system", "content": steps},
+    #                 # {"role": "system", "content": tables_data},
+    #                 *conversation_history
+    #             ]
+    #         )
+    
+    # print(response['usage'])
+    response = third_model(user_message)
     # print(conversation_history)
     
      # Extract the assistant's reply from the API response
-    assistant_reply = response['choices'][0]['message']['content']
+    # assistant_reply = response['choices'][0]['message']['content']
+    assistant_reply = response
 
     # Add the assistant's reply to the conversation history
     conversation_history.append({"role": "assistant", "content": assistant_reply})
+
+    # conversation_history = []
 
     # Update the conversation history in the session
     request.session['conversation_history'] = conversation_history
--- chatbot/chatbot_app/chatbot_app/config.py
+++ chatbot/chatbot_app/chatbot_app/config.py
@@ -2,6 +2,7 @@ from decouple import config
 
 
 OPENAI_KEY = config('OPENAI_KEY')
+MODEL_NAME = config('MODEL_NAME')
 DB_NAME = config('DB_NAME')
 USER = config('USER')
 PASSWORD = config('PASSWORD')
